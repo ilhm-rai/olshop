8da28e2ce88ef6192c0f2991797fb05ec00f45d9
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2020-07-28 08:12:02 +0700

    Remove construct function

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,12 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('User_model');
-        $this->load->library('form_validation');
-    }
 
     public function index()
     {
